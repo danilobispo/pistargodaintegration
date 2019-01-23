949a3e8b81d0f0cb8062f18ea56099b44ef65951
danilobispo <danilobispogalvao@gmail.com>
2019-01-23 16:18:23 -0200

    Melhoria da especificação do seletor de css para remoção de Role e Agent

--- src/main/webapp/persona/persona.js
+++ src/main/webapp/persona/persona.js
@@ -2,8 +2,8 @@ $("#personaButton").click(function() {
 
     // TODO: Remover botões que não fazem parte do CGM proposto para o Persona-based modelling
     // por exemplo: Role e Agent, Contribution Links, Resource, etc
-    $('li:has(a[id="addRole"])').remove();
-    $('li:has(a[id="addAgent"])').remove();
+    $('li[parent="#addActorDropdown"]:has(a[id="addRole"])').remove();
+    $('li[parent="#addActorDropdown"]:has(a[id="addAgent"])').remove();
     // Role e Agent não fazem parte
 
 
