570a966209587d786d4131634164249c05cc0a9f
danilobispo <danilobispogalvao@gmail.com>
2019-01-23 16:19:28 -0200

    Substituição de remove para hide()

--- src/main/webapp/persona/persona.js
+++ src/main/webapp/persona/persona.js
@@ -2,8 +2,8 @@ $("#personaButton").click(function() {
 
     // TODO: Remover botões que não fazem parte do CGM proposto para o Persona-based modelling
     // por exemplo: Role e Agent, Contribution Links, Resource, etc
-    $('li[parent="#addActorDropdown"]:has(a[id="addRole"])').remove();
-    $('li[parent="#addActorDropdown"]:has(a[id="addAgent"])').remove();
+    $('li[parent="#addActorDropdown"]:has(a[id="addRole"])').hide();
+    $('li[parent="#addActorDropdown"]:has(a[id="addAgent"])').hide();
     // Role e Agent não fazem parte
 
 
