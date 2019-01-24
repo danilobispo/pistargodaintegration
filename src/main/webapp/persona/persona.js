820b1f5f048ab779de2f2326dfe93a9894ddbf33
danilobispo <danilobispogalvao@gmail.com>
2019-01-24 10:41:11 -0200

    Início da criação de botões relacionados ao persona based modelling, como avançar para a criação de contextos

--- src/main/webapp/persona/persona.js
+++ src/main/webapp/persona/persona.js
@@ -1,4 +1,8 @@
 /**
+ * Arquivo criado para encapsular todas as funções javascript que envolvam o Persona-Based Modelling
+ */
+
+/**
  * Responsável por remover botões que não fazem parte do CGM proposto para o Persona-based modelling
  */
 function alterarControlesDaUI(){
@@ -9,6 +13,13 @@ function alterarControlesDaUI(){
     $('li[parent="#addActorDropdown"]:has(a[id="addRole"])').hide();
     $('li[parent="#addActorDropdown"]:has(a[id="addAgent"])').hide();
 
+    // Actor links não serão utilizados
+    $('#dropdownMenuActorLink').hide();
+
+    //Contribution Link (softgoals podem entrar novamente, só não serão considerados no CGM
+    // o famoso "peso morto")
+    $('#dropdownMenuContributionLink').hide();
+
 };
 
 /**
@@ -17,6 +28,9 @@ function alterarControlesDaUI(){
 function restaurarControlesDaUI() {
     $('li[parent="#addActorDropdown"]:has(a[id="addRole"])').show();
     $('li[parent="#addActorDropdown"]:has(a[id="addAgent"])').show();
+
+    $('#dropdownMenuActorLink').show();
+    $('#dropdownMenuContributionLink').show();
 };
 
 /**
@@ -25,6 +39,47 @@ function restaurarControlesDaUI() {
  */
 function removerDiagramaAtual() {
     istar.clearModel();
+}
+
+$('#personaArea').on('show.bs.collapse', function () {
+    $('#personaButton').addClass('active');
+    alterarControlesDaUI();
+});
+$('#personaArea').on('hide.bs.collapse', function () {
+    $('#personaButton').removeClass('active');
+    restaurarControlesDaUI();
+});
+
+$('#finishCGMButton').click(function () {
+    let model = JSON.parse(saveModel());
+    let i = 0;
+    for(i; i < model.links.length; i++){
+        console.log(model.links[i]);
+    }
+
+    // TODO: Descobrir como gravar/ler o JSON no Local Storage
+    /*var localStorage = unsafeWindow.localStorage;
+    if(localStorage.length > 0){
+        console.log("kkk n tá vazio");
+    } else {
+        console.log("tá vazio");
+    }*/
+
+});
+
+/**
+ * Cria a árvore CGM que será analisada no passo de achievability
+ * @param model
+ */
+function montarArvoreCGM(model) {
+    let i = 0;
+    // Pega todos os nós d
+
+    let linksTarget;
+    let linksSource;
+    for(i; i < model.nodes.length; i++){
+
+    }
 }
 
 
